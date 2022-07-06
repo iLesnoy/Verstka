1f30de5aaa09d08aeeca104a11a0cead938e179b
Yaraslau <gamasileusbed@gmail.com>
2022-07-06 17:16:05 +0300

    main

--- script/scroll.js
+++ script/scroll.js
@@ -1,37 +1,57 @@
 var listElm = document.querySelector('.main-body');
+function fileRead(i) {
+  fetch("../data/main.json")
+    .then(response => response.json())
+    .then(data => {
+      const test = data.icons.find(item => item.id === i);
+      Object.entries(test).forEach(key => {
+        console.log(test["id"]);
+        console.log(test["image"]);
+        console.log(test["description"]);
+        console.log(test["price"]);
+      })
+      console.log(test);
+      return test;
+    })
+}
 
 function loadMore() {
-  for (var i = 0; i < 20; i++) {
+
+  for (var i = 0; i < 5; i++) {
+
+    fileRead(i);
+    
     var tovar = document.createElement('div');
-    tovar.setAttribute('class','tovar'); 
+    tovar.setAttribute('class', 'tovar');
     var icon_image = document.createElement('div');
-    icon_image.setAttribute('class','icon-image');
+    icon_image.setAttribute('class', 'icon-image');
+    icon_image.innerHTML="<img src='/images/Kroxigor-Lizardmen-Warhammer-Fantasy-фэндомы-4073908.jpeg' class='icon-image'>";
 
     var icon_doby = document.createElement('div');
     var text = document.createElement('p');
     text.innerText = "Coupone Name"
 
     var href = document.createElement('a');
-    href.setAttribute('href','#'); 
-    href.innerHTML ="<img src='/images/favorite_black_24dp.svg' />";
-  
+    href.setAttribute('href', '#');
+    href.innerHTML = "<a href='register.html'><img src='/images/favorite_black_24dp.svg' /></a>";
+
 
     var description = document.createElement('p');
-    description.setAttribute('class','descr-style');
+    description.setAttribute('class', 'descr-style');
     description.innerText = "Some Description";
 
     var line = document.createElement('div');
-    line.setAttribute('class','line');
+    line.setAttribute('class', 'line');
 
     var price = document.createElement('p');
-    price.setAttribute('class','price');
+    price.setAttribute('class', 'price');
     price.innerText = "500$";
 
     var button = document.createElement('button');
-    button.setAttribute('class','add-button');
+    button.setAttribute('class', 'add-button');
     button.innerText = "add"
 
-    icon_doby.setAttribute('class','icon-body');
+    icon_doby.setAttribute('class', 'icon-body');
     icon_doby.appendChild(text);
     icon_doby.appendChild(href);
     icon_doby.appendChild(description);
@@ -43,7 +63,7 @@ function loadMore() {
     tovar.appendChild(icon_image);
     tovar.appendChild(icon_doby);
     listElm.appendChild(tovar);
-   
+
     console.log(tovar);
     return tovar;
   }
@@ -52,20 +72,21 @@ function loadMore() {
 function getDocHeight() {	// $(document).height() value depends on browser
   var D = document;
   return Math.max(
-      D.body.scrollHeight, D.documentElement.scrollHeight,
-      D.body.offsetHeight, D.documentElement.offsetHeight,
-      D.body.clientHeight, D.documentElement.clientHeight
+    D.body.scrollHeight, D.documentElement.scrollHeight,
+    D.body.offsetHeight, D.documentElement.offsetHeight,
+    D.body.clientHeight, D.documentElement.clientHeight
   );
 }
-document.addEventListener('DOMContentLoaded', function(e) {
-  document.addEventListener('scroll', function(e) {
-      let documentHeight = document.body.scrollHeight;
-      let currentScroll = window.scrollY + window.innerHeight;
-      // When the user is [modifier]px from the bottom, fire the event.
-      let modifier = 200; 
-      if(currentScroll + modifier > documentHeight) {
-          console.log('You are at the bottom!')
-          loadMore();
-      }
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  document.addEventListener('scroll', function (e) {
+    let documentHeight = document.body.scrollHeight;
+    let currentScroll = window.scrollY + window.innerHeight;
+    // When the user is [modifier]px from the bottom, fire the event.
+    let modifier = 200;
+    if (currentScroll + modifier > documentHeight) {
+      console.log('You are at the bottom!')
+      loadMore();
+    }
   })
 })
---
+++ script/top.js
@@ -0,0 +1,32 @@
+const showOnPx = 50;
+const backToTopButton = document.querySelector(".back-to-top");
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth"
+  });
+};
+
+document.addEventListener("scroll", () => {
+  console.log("Scroll Height: ", scrollContainer().scrollHeight);
+  console.log("Client Height: ", scrollContainer().clientHeight);
+
+ 
+
+
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+backToTopButton.addEventListener("click", goToTop);
+
+$(document).ready(function(){
+    $(this).scrollTop(0);
+});
